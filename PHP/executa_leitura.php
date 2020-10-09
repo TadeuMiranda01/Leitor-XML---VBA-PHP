e56803f062bb7685d1b669fdad0326364f518af1
HigorPimentel <higorpimentel@hotmail.com>
2020-10-09 16:57:08 -0300

    alteracao php executa leitura

--- PHP/executa_leitura.php
+++ PHP/executa_leitura.php
@@ -1,18 +1,67 @@
 <?php 
 
-$var = $_POST["var_php"];
+//$var = $_POST["var_php"];
 
-
-
-//$doc = new DOMdocument();
-//$doc->load('xml/SEM_FORMATACAO.xml');
 $file_open = fopen('xml/SEM_FORMATACAO.xml', "r");
 $data = fgetcsv($file_open,10000);
-$data_txt = str_replace('"', '' , $data[0]); 
-// Optei em não usar a biblioteca pra evitar erro com a decodificação do arquivo xml 
-//$obj_xml = simplexml_load_string($data_txt);
+$string_xml =  $data[0]; 
+$qtd_caract = strlen($string_xml);
+
+
+$i = 0;
+$dados = $string_xml;
+$chave_abertura = '<';
+$chave_fechamento = '>';
+
+//$dados = '<abertura><informacao><tag2><tag3><tagggg4>dasdsodaosdoasdoa</informacao></abertura>';
+
+for ($i=0; $i < $qtd_caract; $i++) { 
+
+
+//EXTRAIR TEXTO 
+$posicao_tag_abertura = stripos($dados, $chave_abertura);
+$posicao_tag_fechamento = stripos($dados, $chave_fechamento);
+$tag_atual[]  = substr($dados, $posicao_tag_abertura +1, $posicao_tag_fechamento - 1);
+
+$tag_fechamento = substr($dados, $posicao_tag_abertura +1,1);
+$identifica_tag_pai = substr($dados, $posicao_tag_fechamento + 1, 1);
+
+
+if (empty($tag_atual[$i])) {
+	break;
+}
 
 
 
-echo(print_r($data[0]));
+
+
+
+$array_dados_teste[] = $identifica_tag_pai;
+
+
+
+// EXCLUSÃO DE ALGUMAS TAGS
+	if ($tag_fechamento != '/') {
+		
+
+
+
+		$tags[] = array('tg_nome' =>  $tag_atual[$i],
+						 'tg_valor'	=> "vazio",
+	 					 'tg_tipo'	=> "vazio" );
+
+
+
+	}
+
+
+
+
+
+$dados = substr($dados, $posicao_tag_fechamento + 1);
+}
+
+
+
+echo(print_r($array_dados_teste));
 ?>
